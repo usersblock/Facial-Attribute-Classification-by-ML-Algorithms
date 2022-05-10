c88d60da7a08146c5800633a6bd09ba8f4fa525c
Jenifer Vivar <jvivar000@citymail.cuny.edu>
2022-05-10 14:02:03 -0400

    Update JV-ImagesProcessingFunctions.py
    splited the functions to only return one array per function

--- Notebooks/JV-ImagesProcessingFunctions.py
+++ Notebooks/JV-ImagesProcessingFunctions.py
@@ -4,7 +4,6 @@ Spyder Editor
 
 This is a temporary script file.
 """
-
 import pandas as pd
 import numpy as np
 from os import listdir
@@ -19,48 +18,57 @@ from skimage.util import compare_images
 from skimage import exposure
 from skimage import data, img_as_float
 from skimage import exposure
+from skimage.color import rgb2gray
+
 
 @adapt_rgb(each_channel)
 def sobel_each(image):
     return filters.sobel(image)
 
-
 @adapt_rgb(hsv_value)
 def sobel_hsv(image):
     return filters.sobel(image)
 
-def sobelFilterTypes(imagesArray):
-    """
-    This function takes as input an array of pictures and returns two arrays. The first array has the 
-    Sobel filter apply to each picture channel and the second one is applied on the image's hsv. The images are in color
-    """
-    #filter apply to pictures channel
-    filterChannel = []
+
+
+def sobelFilterHSV(imagesArray):
+    """This function takes as input an array of pictures and returns two arrays. The first array has the 
+    Sobel filter apply to each picture channel and the second one is applied on the image's hsv. The images are in color"""
+
     #filter apply to hue, saturation, value
     filterHSV = []
     for image in imagesArray:
-        image1 = rescale_intensity(1 - sobel_each(image))
         image2 = rescale_intensity(1 - sobel_hsv(image))
-        filterChannel.append(image1)
         filterHSV.append(image2)
-    return np.array(filterChannel), np.array(filterHSV)
-        
+    return np.array(filterHSV)
 
-def robertsSobelEdges(imagesArray):
+def sobelChannel(imagesArray):
+    filterChannel = [] 
+    for images in images Array:
+        image1 = rescale_intensity(1 - sobel_each(image))
+        filterChannel.append(image1)
+        
+    return np.array(filterChannel)
+    
+def robertsEdges(imagesArray):
     from skimage import color
-    """
-    returns two numpy arrays with pictures in black and white and with the edges define using Roberts filter
-    on the first one and the Sobel filter on the second one. The images are in black and white
-    """
+    """returns two numpy arrays with pictures in black and white and with the edges define using Roberts filter
+    on the first one and the Sobel filter on the second one. The images are in black and white"""
     roberts_ = []
+    for image in imagesArray:
+        grayImg = rgb2gray(image)
+        edge_roberts = filters.roberts(grayImg)
+        roberts_.append(edge_roberts)
+       
+    return np.array(roberts_)
+
+def sobelEdges(imagesArray):
     sobel_ = []
     for image in imagesArray:
-        grayImg = color.rgb2gray(image)
-        edge_roberts = filters.roberts(grayImg)
+        grayImg = rgb2gray(image)
         edge_sobel = filters.sobel(grayImg)
-        roberts_.append(edge_roberts)
         sobel_.append(edge_sobel)
-    return np.array(roberts_), np.array(sobel_)
+    return np.array(sobel_)
 
 def exposure_(imagesArray):
     from skimage import exposure
@@ -74,52 +82,67 @@ def exposure_(imagesArray):
         exp.append(image_eq)
     return np.array(exp)
 
-def gammaLogaritmictCorrection(imagesArray):
+def gammaCorrection(imagesArray):
     """
     histogram equalizer 
     """
     gamm = []
-    log = []
     for image in imagesArray:
-        gamma_corrected = exposure.adjust_gamma(image, 2)
-        logarithmic_corrected = exposure.adjust_log(image, 1)
+        gamma_corrected = exposure.adjust_gamma(image, 2) 
         gamm.append(gamma_corrected)
+        
+    return np.array(gamm)
+
+def logCorrection(imagesArray):
+    log = []
+    for image in images:
+        logarithmic_corrected = exposure.adjust_log(image, 1)
         log.append(logarithmic_corrected)
-    return np.array(gamm), np.array(log)
+    return np.array(log)
+
 
 def histogramEqualizer(imagesArray):
     """
     returns 3 numpy arrays one is with rescale, equalize histogram, adapthis
     
     """
+    from skimage import color
     
-    
-    
-    intensity = []
     hist = []
-    adapthist = []
-
+    
     for image in imagesArray:
-        p2, p98 = np.percentile(image, (2, 98))
-        
-        img_rescale = exposure.rescale_intensity(image, in_range=(p2, p98))
-
+        image = color.rgb2gray(image)
         # Equalization
         img_eq = exposure.equalize_hist(image)
-
         # Adaptive Equalization
-        img_adapteq = exposure.equalize_adapthist(image, clip_limit=0.03)
-        
-        intensity.append(img_rescale)
         hist.append(img_eq)
+        
+        
+    return np.array(hist)
+
+def intesityEqualizer(imagesArray):
+    intensity = []
+    for images in imagesArray:
+        image = color.rgb2gray(image)
+        p2, p98 = np.percentile(image, (2, 98))
+        img_rescale = exposure.rescale_intensity(image, in_range=(p2, p98))
+        intensity.append(img_rescale)
+    return np.array(intensity)
+
+def adaptistEqualizer(imagesArray):
+    adapthist = []
+    for image in imagesArray:
+        image = color.rgb2gray(image)
+        mg_adapteq = exposure.equalize_adapthist(image, clip_limit=0.03)
         adapthist.append(img_adapteq)
         
-    return np.array(intensity), np.array(hist), np.array(adapthist)
+    return np.array(adapthist)
 
 def images_extractor(mypath):
     import pandas as pd
     from os import listdir
     import numpy as np
+    mypath = '/Users/jenifervivar/Desktop/Desktop - jenifer’s MacBook Air/Machine_learning_code/crop_test/'
  
     #mypath = '/Users/jenifervivar/Desktop/crop_part1/'#'./Data/CroppedImages'
     filenames = np.array([f for f in listdir(mypath) if isfile(join(mypath, f))])
@@ -137,6 +160,23 @@ def images_extractor(mypath):
         images.append(image)
     return images
 
-#robers, sobel_ = robertsSobelEdges(images)
-#intensity, hist, adapthist = histogramEqualizer(images)
-#gamma, log = gammaLogaritmictCorrection(images)
+sobel_hsv = sobelFilterHSV(images)
+sobel_channel = sobelChannel(images)
+robertEdges = robertsEdges(images)
+sobelEdges = sobelEdges(images)
+exposureImage = exposure(images)
+gamma = gammaCorrection(images)
+log = logCorrection(images)
+histE = histogramEqualizer(images)
+intensityEqua = intesityEqualizer(images)
+adaptist = adaptistEqualizer(images)
+
+
+
+
+
+
+
+
+
+
